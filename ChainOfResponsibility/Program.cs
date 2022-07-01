c577db17064f96e7293c74730e06291b956b2ddb
IhorKlymenko <ihorklymenkoc@gmail.com>
2022-07-01 20:56:58 +0300

    Add ChainOfResponsibility pattern

---
+++ ChainOfResponsibility/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ChainOfResponsibility
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Shop shop = new Shop();
+            Console.WriteLine("What do you want to buy? sword, axe, amulet?");
+            string product = Console.ReadLine();
+            Console.WriteLine("It won't be cheap! Don't spare money. (enter amount of money)");
+            int money = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(shop.GetTheJobDone(money, product));
+        }
+    }
+}
