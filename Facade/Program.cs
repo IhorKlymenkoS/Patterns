96bb9b8f057ccf828196efa15220570fc3547752
IhorKlymenko <ihorklymenkoc@gmail.com>
2022-07-01 20:59:51 +0300

    Add Facade pattern

---
+++ Facade/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Facade
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TheWeather weather = new TheWeather();
+            Console.WriteLine(weather.CheckTheWeather());
+        }
+    }
+}
