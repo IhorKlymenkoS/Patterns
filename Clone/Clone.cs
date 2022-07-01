62d7d9ec9c2f98e1474359ce08a1a77c1fb3f13f
IhorKlymenko <ihorklymenkoc@gmail.com>
2022-07-01 21:05:39 +0300

    Add Clone pattern

---
+++ Clone/Clone.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Clone
+{
+    abstract class Prototype
+    {
+        public int Id { get; private set; }
+        public string Color { get; private set; }
+        public string Name { get; private set; }
+
+        public Prototype(int id, string color, string name)
+        {
+            Id = id;
+            Name = name;
+            Color = color;
+        }
+        public abstract Prototype Clone();
+    }
+
+    class ConcretePrototype1 : Prototype
+    {
+        public ConcretePrototype1(int id, string color, string name)
+            : base(id, color, name)
+        { }
+        public override Prototype Clone()
+        {
+            return new ConcretePrototype1(Id, Color, Name);
+        }
+    }
+
+    class ConcretePrototype2 : Prototype
+    {
+        public ConcretePrototype2(int id, string color, string name)
+            : base(id, color, name)
+        { }
+        public override Prototype Clone()
+        {
+            return new ConcretePrototype2(Id, Color, Name);
+        }
+    }
+}
---
+++ Clone/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Clone
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Prototype prototype = new ConcretePrototype1(1, "white", "Dolly sheep");
+            Prototype clone = prototype.Clone();
+            Console.WriteLine(clone.Id + " " + clone.Name + " " + clone.Color);
+            prototype = new ConcretePrototype2(2, "black", "Molly sheep");
+            clone = prototype.Clone();
+            Console.WriteLine(clone.Id + " " + clone.Name + " " + clone.Color);
+        }
+    }
+}
