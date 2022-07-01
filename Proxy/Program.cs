d2899e543abbc32834df650ee346ff6e4db3d1e1
IhorKlymenko <ihorklymenkoc@gmail.com>
2022-07-01 20:52:23 +0300

    Add Proxy pattern

---
+++ Proxy/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Proxy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the amount to be sent");
+            Bank bank = new OneHundredPercentReliability();
+            int money = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("You have received");
+            Console.WriteLine(bank.TransferMoney(money));
+        }
+    }
+}
